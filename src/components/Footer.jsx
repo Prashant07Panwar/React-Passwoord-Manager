beea6a792db48921e9911119f9b429df6e0ff602
Prashant07Panwar <prashantpanwar584@gmail.com>
2025-01-25 12:24:14 +0530

    commit 25/1/2025

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="h-14 bg-[#1e293b] flex justify-between px-3 fixed bottom-0 w-full items-center text-gray-200 font-bold text-lg">
+        <div className="container mx-auto text-center">
+            <p>&copy; 2025 React Password Manager App. All rights reserved.</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
